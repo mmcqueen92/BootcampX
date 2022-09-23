474b4c9f957890bf6300de47bdddee122dcce738
Malcolm McQueen <malcolmmcqueen@live.com>
2022-09-23 02:23:06 +0000

    create students.js and teachers.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`]
+
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
+
+
+
